5d5e53a52b66f9fa29d31b6f1578c953444f8400
yashwantnishad-max <yashwantbit@gmail.com>
2026-04-17 14:20:15 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,27 @@
+
+
+
+//Palindrome nums
+
+let nums = 1331;
+function myFun(n) {
+
+    if (n <= 0) return false;
+
+    let original = n;
+
+    let rev = 0;
+
+    n = Math.abs(n)
+
+    while (n !== 0) {
+
+        let lastDigit = n % 10;
+        rev = rev * 10 + lastDigit;
+        n = Math.floor(n / 10)
+
+    }
+    return original === rev
+
+}
+console.log(myFun(nums))  // true
--- reverse-numbers.js
+++ reverse-numbers.js
@@ -1,40 +1,24 @@
-// Reverse a number
 
+// Reverse a number
 // Time Complexity (TC): O(log10 n)
-// → We process one digit per iteration
-// → Number of digits in n = log10(n)
 
 // Space Complexity (SC): O(1)
-// → No extra space used, only a few variables
 
 function myFun(nums) {
 
-  // Store sign to handle negative numbers
-  let sign = nums < 0 ? -1 : 1;
+    let sign = nums < 0 ? -1 : 1;
+    //sign= 12<0? -1:
+    nums = Math.abs(nums);
 
-  // Work with positive number
-  nums = Math.abs(nums);
+    let rev = 0;
 
-  let rev = 0; // This will store the reversed number
+    while (nums !== 0) {
+        let lastDigit = nums % 10;
+        rev = rev * 10 + lastDigit;
 
-  while (nums !== 0) {
-
-    // Step 1: Get last digit
-    // Example: 1234 % 10 = 4
-    let lastDigit = nums % 10;
-
-    // Step 2: Append last digit to reversed number
-    // Shift rev left (multiply by 10) and add lastDigit
-    // Example: rev = 43 → 43*10 + 2 = 432
-    rev = rev * 10 + lastDigit;
-
-    // Step 3: Remove last digit from nums
-    // Example: 1234 → 123
-    nums = Math.floor(nums / 10);
-  }
-
-  // Restore original sign
-  return rev * sign;
+        nums = Math.floor(nums / 10);
+    }
+    return rev * sign;
 }
 
 // Test cases
