2bdff893f18af9f49d32bf8100e41b542f7a6a65
yashwantnishad-max <yashwantbit@gmail.com>
2026-04-20 12:05:33 +0530

    Move zeros to end / two pointers

--- move-zeros-to-end-optimal.js
+++ move-zeros-to-end-optimal.js
@@ -1,22 +1,25 @@
 
+// Move all Zeros to end (Two Pointers)
 
-let arr=[1,0,2,3,0,4]
+let arr = [1, 0, 2, 3, 0, 4]
 
+// TC= O(n)
+// SC= O(1)
+function myFun(nums) {
 
-function myFun(nums){
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
 
-    let j=0;
-    for(let i=0; i<nums.length; i++){
-
-        if(nums[i]!==0){
-            let temp=nums[i]
-             nums[i]=nums[j]
-             nums[j]=temp
-             j++
+        if (nums[i] !== 0) {
+            let temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            j++
         }
     }
-     
+
     return nums
 
 }
-console.log(myFun(arr))
+console.log(myFun(arr))    // [1, 2, 3, 4, 0, 0]
+
