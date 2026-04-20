4b301b3ebb5a0c61704a8d24b9d140a74fe46f8a
yashwantnishad-max <yashwantbit@gmail.com>
2026-04-20 10:02:52 +0530

    Reverse an Array

---
+++ revers-an-array-in-place.js
@@ -0,0 +1,23 @@
+
+
+//Reverse array
+
+//tc= O(n)
+//sc= O(1)
+
+function myFun(nums){
+  
+  let left=0;
+  let right=nums.length-1;
+  
+  while(right>left){
+    [nums[left], nums[right]]=[nums[right], nums[left]]
+    left++
+    right--
+  }
+  return nums
+}
+console.log(myFun([1,2,3,4,5,6])) // [6,5,4,3,2,1]
+
+
+
